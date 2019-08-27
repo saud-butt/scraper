50909fa56f0033b353ef0d3356b107de1cdea447
Fakhir Shad <fakhir.shad@hotmail.com>
2019-08-27 12:09:28 +0500

    #added msi scrapper

--- scrapper.js
+++ scrapper.js
@@ -1,23 +1,95 @@
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
-const url = "https://www.reddit.com/r/news/";
+const url =
+  "https://www.msi.com/Laptop/Products#?tag_multi_select=579,580,581,582,583,2011";
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
-  const html = await page.evaluate(() => document.body.innerHTML);
-
-  const $ = cheerio.load(html);
-  const newsHeadlines = [];
-
-  $('a[href*="/r/news/comments"]  > div > h3').each(function() {
-    newsHeadlines.push({
-      title: $(this).text()
-    });
-  });
-
-  console.log(newsHeadlines);
+  let obj = [];
+  let hasNextPage = true;
+  while (hasNextPage) {
+    await page.waitFor(1000);
+    const pageDetail = await prepareNextPage(page);
+    hasNextPage = pageDetail.hasNextPage;
+    const pageLinks = await getProductionLinks(pageDetail.html);
+    obj = [...obj, ...pageLinks];
+    if (hasNextPage) {
+      await page.click(
+        `#mainbox2 > ul.pagination > li:not(.last_li):nth-child(${pageDetail.nextLinkIndex}) > a`
+      );
+    }
+  }
+  let productDetails = [];
+  for (let link of obj) {
+    await page.goto(`${link}/Specification`);
+    await page.waitFor(1000);
+    const html = await page.evaluate(() => document.body.innerHTML);
+    productDetails = [...productDetails, ...(await getProductDetails(html))];
+  }
+  console.log("Objects", productDetails);
+  console.log("Count", productDetails.length);
   await browser.close();
 })();
+
+async function prepareNextPage(page) {
+  return page.evaluate(() => {
+    const nodes = document.body
+      .querySelector(".pagination")
+      .querySelectorAll("li:not(.last_li)");
+    const nextLinkIndex = nodes.length;
+    const nextLink = nodes[nextLinkIndex - 1];
+    return {
+      hasNextPage: !nextLink.classList.value.includes("active"),
+      html: document.body.querySelector("#product_box_list").innerHTML,
+      nextLinkIndex
+    };
+  });
+}
+
+async function getProductionLinks(html) {
+  const links = [];
+  const $ = cheerio.load(html);
+  $("a.productcard-link").each(function() {
+    const link = $(this).attr("href");
+    if (!links.includes(link)) {
+      links.push($(this).attr("href"));
+    }
+  });
+  return links;
+}
+
+async function getProductDetails(html) {
+  const obj = [];
+  const $ = cheerio.load(html);
+  const ths = $("thead")
+    .find("tr")
+    .find("td:not(.info)");
+  ths.each(function(index, td) {
+    obj.push({
+      model: $(td)
+        .text()
+        .trim()
+    });
+  });
+  const tr = $("tbody").find("tr");
+  tr.each(function(index, element) {
+    const tds = $(element).find("td");
+    tds.each(function(index, td) {
+      const span = $(td).find("span");
+      const text = span.text();
+      const key = text
+        .toLowerCase()
+        .trim()
+        .split(" ")
+        .join("_");
+      obj[index][key] = $(td)
+        .text()
+        .replace(text, "")
+        .trim();
+    });
+  });
+  return obj;
+}
